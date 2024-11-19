4ca6950814a23aa789a5cf529b4fe14d543e3f42
Anis Novi <anisnovi811.@gmail.com>
2024-11-19 08:25:51 +0700

    welcome and home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,83 +9,145 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Starter Page</h1>
+                    <div class="page-pretitle">
+                        Home
+                    </div>
+            <h1 class="m-0">Dashboard</h1>
           </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item active">Home Page</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
 
-    <!-- Main content -->
-    <div class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-lg-6">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
 
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's
-                  content.
-                </p>
+         <!-- Page body -->
+    <div class="page-body">
+        <div class="container-xl">
+            <div class="row row-deck row-cards mb-4">
 
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-              </div>
+                <div class="col-12">
+                    <div class="row row-cards">
+                        <div class="col-sm-6 col-lg-3">
+                            <div class="card card-sm">
+                                <div class="card-body">
+                                    <div class="row align-items-center">
+                                        <div class="col-auto">
+                                            <span
+                                                class="bg-primary text-white avatar"><!-- Download SVG icon from http://tabler-icons.io/i/currency-dollar -->
+
+                                                <i class="ti ti-stack-2 icon"></i>
+                                            </span>
+                                        </div>
+                                        <div class="col">
+                                            <div class="font-weight-medium">
+                                               1
+                                            </div>
+                                            <div class="text-secondary">
+                                                Produk
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-3">
+                            <div class="card card-sm">
+                                <div class="card-body">
+                                    <div class="row align-items-center">
+                                        <div class="col-auto">
+                                            <span
+                                                class="bg-green text-white avatar"><!-- Download SVG icon from http://tabler-icons.io/i/shopping-cart -->
+
+                                                <i class="ti ti-transfer-in icon"></i>
+                                            </span>
+                                        </div>
+                                        <div class="col">
+                                            <div class="font-weight-medium">
+                                              5
+                                            </div>
+                                            <div class="text-secondary">
+                                                Barang Masuk
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-3">
+                            <div class="card card-sm">
+                                <div class="card-body">
+                                    <div class="row align-items-center">
+                                        <div class="col-auto">
+                                            <span class="bg-twitter text-white avatar">
+                                                <i class="ti ti-transfer-out icon"></i>
+                                            </span>
+                                        </div>
+                                        <div class="col">
+                                            <div class="font-weight-medium">
+                                             7
+                                            </div>
+                                            <div class="text-secondary">
+                                                Barang Keluar
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+
+
+                <div class="col-12">
+                    <div class="card card-md">
+                        <div class="card-stamp card-stamp-lg">
+                            <div class="card-stamp-icon bg-primary">
+                                <!-- Download SVG icon from http://tabler-icons.io/i/ghost -->
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                    stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path
+                                        d="M5 11a7 7 0 0 1 14 0v7a1.78 1.78 0 0 1 -3.1 1.4a1.65 1.65 0 0 0 -2.6 0a1.65 1.65 0 0 1 -2.6 0a1.65 1.65 0 0 0 -2.6 0a1.78 1.78 0 0 1 -3.1 -1.4v-7" />
+                                    <path d="M10 10l.01 0" />
+                                    <path d="M14 10l.01 0" />
+                                    <path d="M10 14a3.5 3.5 0 0 0 4 0" />
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col-10">
+                                    <h3 class="h1">Welcome, {{ auth()->user()->name }}</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
 
-            <div class="card card-primary card-outline">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+            <div class="row row-cards">
+                <div class="col-12 col-lg-4">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Barang Masuk Terakhir</h3>
+                        </div>
+                        <div class="list-group list-group-flush list-group-hoverable">
+                           isi codingan
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Barang Keluar Terakhir</h3>
+                        </div>
+                        <div class="list-group list-group-flush list-group-hoverable">
+                          isi codingan
+                        </div>
+                    </div>
+                </div>
 
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's
-                  content.
-                </p>
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-              </div>
-            </div><!-- /.card -->
-          </div>
-          <!-- /.col-md-6 -->
-          <div class="col-lg-6">
-            <div class="card">
-              <div class="card-header">
-                <h5 class="m-0">Featured</h5>
-              </div>
-              <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
-
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-
-            <div class="card card-primary card-outline">
-              <div class="card-header">
-                <h5 class="m-0">Featured</h5>
-              </div>
-              <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
-
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-          <!-- /.col-md-6 -->
-        </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-
+</div>  
+</div>
+</div>  
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,55 +1,194 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('adminlte.layouts.auth')
+
+@section('content')
+
+<!doctype html>
+<!--
+* Tabler - Premium and Open Source dashboard template with responsive and high quality UI.
+* @version 1.0.0-beta20
+* @link https://tabler.io
+* Copyright 2018-2023 The Tabler Authors
+* Copyright 2018-2023 codecalm.net Paweł Kuna
+* Licensed under MIT (https://github.com/tabler/tabler/blob/master/LICENSE)
+-->
+<html lang="en">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Data Inventory Barang</title>
+    <!-- CSS files -->
+    <!-- Filepond stylesheet -->
+    <link href="https://unpkg.com/filepond/dist/filepond.css" rel="stylesheet">
+    <link href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css"
+        rel="stylesheet" />
 
-    <title>Data Barang</title>
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css">
+    <link href="{{ asset('dist/css/tabler.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('dist/css/tabler-flags.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('dist/css/tabler-payments.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('dist/css/tabler-vendors.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('dist/css/demo.min.css') }}" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet">
+  
     <style>
+        @import url('https://rsms.me/inter/inter.css');
+
+        :root {
+            --tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+        }
+
         body {
-            font-family: 'Nunito', sans-serif;
+            font-feature-settings: "cv03", "cv04", "cv11";
         }
     </style>
 </head>
-<body class="antialiased">
-    <div class="container mt-5">
-        <h1>Data Barang</h1>
 
-        @if (Route::has('login'))
-            <div class="mb-4">
-                @auth
-                    <a href="{{ url('/home') }}" class="btn btn-primary">Home</a>
-                @else
-                    <a href="{{ route('login') }}" class="btn btn-secondary">Log in</a>
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="btn btn-secondary ml-4">Register</a>
-                    @endif
-                @endauth
+<body>
+    <script src="{{ asset('dist/js/demo-theme.min.js') }}"></script>
+    <div class="page">
+        <!-- Navbar -->
+        <header class="navbar navbar-expand-md d-print-none">
+            <div class="container-xl">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu"
+                    aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
+                    <a href=".">
+                        {{-- <img src="./static/logo.svg" width="110" height="32" alt="Tabler"
+                            class="navbar-brand-image"> --}}
+                        DATA BARANG
+                    </a>
+                </h1>
+                <div class="navbar-nav flex-row order-md-last">
+                    <div class="d-none d-md-flex">
+                                    
+                    @if (Route::has('login'))
+                                <div class="mb-4">
+                                    @auth
+                                        <a href="{{ url('/home') }}" class="btn btn-primary">Home</a>
+                                    @else
+                                        <a href="{{ route('login') }}" class="nav-link px-3">Log in</a>
+                                        @if (Route::has('register'))
+                                            <a href="{{ route('register') }}" class="nav-link px-3">Register</a>
+                                        @endif
+                                    @endauth
+                                </div>
+                            @endif
+                       
+
+                    </div>
+
+                </div>
+                <div class="collapse navbar-collapse" id="navbar-menu">
+                    <div class="d-flex flex-column flex-md-row flex-fill align-items-stretch align-items-md-center">
+                        <ul class="navbar-nav">
+
+                        </ul>
+                    </div>
+                </div>
             </div>
-        @endif
+        </header>
 
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>Nama Barang</th>
-                    <th>Stok</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($barangs as $barang)
-                <tr>
-                    <td>{{ $barang->nama_barang }}</td>
-                    <td>{{ $barang->stok }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+ <!-- Page body -->
+ <div class="page-body">
+        <div class="container-xl">
+
+            <div class="row">
+                <div class="col-12 col-lg-4">
+                    <form action="" method="get">
+                        <div class="input-icon mb-3">
+                            <input type="search" value="{{ request()->query('keyword') }}" class="form-control w-100"
+                                name="keyword" placeholder="Search…">
+
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+            <div class="row row-deck row-cards">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            {{-- Total : {{ $barangKeluarDetails->count() }} --}}
+                        </div>
+                        <div class="table-responsive">
+                            <table class="table table-vcenter table-mobile-md card-table">
+                                <thead>
+                                    <tr>
+                                        <th class="w-1">No</th>
+                                        <th>Barang</th>
+                                        <th class="text-center">Stok</th>
+                                        <th class="text-center">Masuk</th>
+                                        <th class="text-center">Keluar</th>
+
+
+                                        {{-- <th class="w-1"></th> --}}
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    @foreach ($barangs as $barang)
+                                        <tr>
+                                            <td class="align-text-top text-start text-lg-center" data-label="id">
+                                            {{ $barang->id }}
+                                            </td>
+                                            <td class="align-text-top text-start text-lg-center" data-label="nama_barang">
+                                            {{ $barang->nama_barang }}
+                                            </td>
+                                           
+                                            <td class="align-text-top text-start text-lg-center" data-label="Stok">
+                                            {{ $barang->stok }}
+                                            </td>
+                                            
+                                        </tr>
+                                    @endforeach
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>                           
+
     </div>
-</body>
+
+    <!-- Libs JS -->
+    <script src="{{ asset('dist/libs/tom-select/dist/js/tom-select.base.min.js') }}" defer></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+    <script>
+        toastr.options = {
+            "closeButton": false,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": true,
+            "positionClass": "toast-top-center",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        }
+    </script>
+
+    <!-- Tabler Core -->
+    <script src="{{ asset('dist/js/tabler.min.js') }}" defer></script>
+    <script src="{{ asset('dist/js/demo.min.js') }}" defer></script>
+
+    @stack('custom_script')
+   
+    </body>
 </html>
