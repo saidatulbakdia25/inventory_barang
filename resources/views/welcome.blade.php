c8ad9f6e170c9085e575068e00b102a60cdd7402
Anis Novi <anisnovi811.@gmail.com>
2024-11-19 10:16:30 +0700

    hapus link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,53 +2,6 @@
 
 @section('content')
 
-<!doctype html>
-<!--
-* Tabler - Premium and Open Source dashboard template with responsive and high quality UI.
-* @version 1.0.0-beta20
-* @link https://tabler.io
-* Copyright 2018-2023 The Tabler Authors
-* Copyright 2018-2023 codecalm.net Paweł Kuna
-* Licensed under MIT (https://github.com/tabler/tabler/blob/master/LICENSE)
--->
-<html lang="en">
-
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Data Inventory Barang</title>
-    <!-- CSS files -->
-    <!-- Filepond stylesheet -->
-    <link href="https://unpkg.com/filepond/dist/filepond.css" rel="stylesheet">
-    <link href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css"
-        rel="stylesheet" />
-
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css">
-    <link href="{{ asset('dist/css/tabler.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('dist/css/tabler-flags.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('dist/css/tabler-payments.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('dist/css/tabler-vendors.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('dist/css/demo.min.css') }}" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet">
-  
-    <style>
-        @import url('https://rsms.me/inter/inter.css');
-
-        :root {
-            --tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
-        }
-
-        body {
-            font-feature-settings: "cv03", "cv04", "cv11";
-        }
-    </style>
-</head>
-
-<body>
-    <script src="{{ asset('dist/js/demo-theme.min.js') }}"></script>
     <div class="page">
         <!-- Navbar -->
         <header class="navbar navbar-expand-md d-print-none">
@@ -64,26 +17,24 @@
                         DATA BARANG
                     </a>
                 </h1>
-                <div class="navbar-nav flex-row order-md-last">
-                    <div class="d-none d-md-flex">
-                                    
-                    @if (Route::has('login'))
-                                <div class="mb-4">
-                                    @auth
-                                        <a href="{{ url('/home') }}" class="btn btn-primary">Home</a>
-                                    @else
-                                        <a href="{{ route('login') }}" class="nav-link px-3">Log in</a>
-                                        @if (Route::has('register'))
-                                            <a href="{{ route('register') }}" class="nav-link px-3">Register</a>
-                                        @endif
-                                    @endauth
-                                </div>
-                            @endif
+                <div class="btn-group">
+                   
+                                    @if (Route::has('login'))
+                                        <div class="p-3 border bg-light">
+                                            @auth
+                                                <a href="{{ url('/home') }}" class="btn btn-primary">Home</a>
+                                            @else
+                                                <a href="{{ route('login') }}" class="btn btn-primary active" aria-current="page">Log in</a>
+                                                @if (Route::has('register'))
+                                                    <a href="{{ route('register') }}" class="btn btn-primary active" aria-current="page">Register</a>
+                                                @endif
+                                            @endauth
+                                        </div>
+                                    @endif
                        
-
-                    </div>
-
-                </div>
+             </div>
+                                    
+                  
                 <div class="collapse navbar-collapse" id="navbar-menu">
                     <div class="d-flex flex-column flex-md-row flex-fill align-items-stretch align-items-md-center">
                         <ul class="navbar-nav">
@@ -158,37 +109,3 @@
         </div>                           
 
     </div>
-
-    <!-- Libs JS -->
-    <script src="{{ asset('dist/libs/tom-select/dist/js/tom-select.base.min.js') }}" defer></script>
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
-        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
-    <script>
-        toastr.options = {
-            "closeButton": false,
-            "debug": false,
-            "newestOnTop": false,
-            "progressBar": true,
-            "positionClass": "toast-top-center",
-            "preventDuplicates": false,
-            "onclick": null,
-            "showDuration": "300",
-            "hideDuration": "1000",
-            "timeOut": "5000",
-            "extendedTimeOut": "1000",
-            "showEasing": "swing",
-            "hideEasing": "linear",
-            "showMethod": "fadeIn",
-            "hideMethod": "fadeOut"
-        }
-    </script>
-
-    <!-- Tabler Core -->
-    <script src="{{ asset('dist/js/tabler.min.js') }}" defer></script>
-    <script src="{{ asset('dist/js/demo.min.js') }}" defer></script>
-
-    @stack('custom_script')
-   
-    </body>
-</html>
